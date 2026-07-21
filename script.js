a0154a773564408b5935821592ed862414b5e641
Vinicius Jesus <vinnyjesus62@gmail.com>
2026-07-21 13:42:09 -0300

    counter App

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const increaseBtn = document.querySelector('.increase-btn');
+const decreaseBtn = document.querySelector('.decrease-btn');
+const counterHeading = document.querySelector(".counter"); 
+
+let counter = 0;
+
+increaseBtn.addEventListener('click', () => {
+    counter += 1;
+    counterHeading.textContent = counter;
+});
+decreaseBtn.addEventListener('click', () => {
+    counter -= 1;
+
+    if (counter <= 0) {
+        counter = 0;
+    }
+    counterHeading.textContent = counter;
+});
